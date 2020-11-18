54893876923b2f3f391fbaa242ecc693a0da9a43
David Wallace <dwallacemarketing@gmail.com>
2020-11-17 21:30:51 -0500

    additional updates to fetch questions to console. question card ccomponent created. plan to display answers via pop up module

--- src/Components/Questions/ViewQuestion.js
+++ src/Components/Questions/ViewQuestion.js
@@ -1,37 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState, EFf, useEffect } from 'react';
 import { } from 'reactstrap';
 
 const ViewQuestion = (props) => {
-
+    console.log('VIEWING QUESTIONS ')
     // fetch to display questions 
     //.map statement to show the question fetched from the object. possible pagiation ?
 
     // console.log('question token -->', props.token)
 
     // const [submittedQuestion, setSubmittedQuestion] = useState(false);
+    
+    useEffect(() => {
 
-    const viewQuestion = (e) => {
-        // e.preventDefault();
+        const fetchQuestions = (e) => {
+            // e.preventDefault();
+    
+            fetch("http://localhost:5000/question/", {
+                method: 'GET',
+                headers: new Headers({
+                    // 'Content-Type': 'application/json',
+                    'Authorization': props.token
+                })
+            })
+                .then(response => response.json())
+                .then(result => {
+                    console.log('view questions---->', result)
 
-        fetch("http://localhost:5000/question/", {
-            method: 'GET',
-            headers: new Headers({
-                // 'Content-Type': 'application/json',
-                'Authorization': props.token
-            })
-        })
-            .then(response => response.json())
-            .then(result => {
-                console.log('view questions---->', result)
-                // setSubmittedQuestion(true);
-            })
-            .catch(error => console.log('error', error));
-    }
+                })
+                .catch(error => console.log('error', error));
+        }
+    
+        fetchQuestions();
+
+
+    }, [])
+   
 
     return (
         <div>
             <h3>View Questions</h3>
-            {viewQuestion()}
         </div>
     )
 }
