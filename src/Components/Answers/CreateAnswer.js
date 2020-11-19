9b1647176ad3d92c5b2ba88bc3eff7b6918e5ebd
DeShawne <dmw606@gmail.com>
2020-11-19 17:57:57 -0500

    create answer component minus ternary

--- src/Components/Answers/CreateAnswer.js
+++ src/Components/Answers/CreateAnswer.js
@@ -1,13 +1,54 @@
 import React, {useState} from 'react';
-import { } from 'reactstrap';
+import {Button, Form, Label, Input} from 'reactstrap';
 
 const CreateAnswer = (props) => {
-    return (
+    console.log('answer token -->', props.token)
+
+    const [answerTitle, setAnswerTitle] = useState('');
+    const [answerLikes, setAnswerLikes] = useState('');
+    const [answerEntry, setAnswerEntry] = useState('');
+    const [submittedAnswer, setSubmittedAnswer] = useState(false);
+    const [answerLiked, setAnswerLiked] = useState(false);
+
+    const submitQuestion = (e) => {
+        e.preventDefault();
+
+        fetch('http://localhost:8080/answer/', {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            }),
+            body: JSON.stringify({
+                "answer": {
+                    "title": answerTitle,
+                    "entry": answerEntry,
+                    "likes": answerLikes
+                }
+            })
+        })
+        .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                setSubmittedAnswer(true);
+            })
+            .catch(error => console.log('error', error));
+   
+}
+
+
+ return (
+     <div>
+         {
+             submittedAnswer === true ?
+             
+         }
         <div>
             <h3>CreateAnswer</h3>
         
         </div>
+    </div>
     )
-}
+
 
 export default CreateAnswer;
