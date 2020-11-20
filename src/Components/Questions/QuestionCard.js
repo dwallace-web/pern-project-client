9ff82203c36592efdad999acc4ac6ac17334a9c7
David Wallace <dwallacemarketing@gmail.com>
2020-11-19 19:20:33 -0500

    create frame for question card

--- src/Components/Questions/QuestionCard.js
+++ src/Components/Questions/QuestionCard.js
@@ -1,14 +1,30 @@
 import React from 'react'
-
+import { Button } from 'reactstrap';
 
 const QuestionCard = (props) => {
 
 
     return (
         <div>
-            {props.questionRes.map(question =>
+            {props.questionRes.map(question => {
+
                 console.log('question card', question)
-                )}
+
+                return(
+
+                    <div key={question.id}>
+                        <h2>  {question.title} </h2>
+                        <h3>{question.category} </h3>
+                        {question.entry}
+                        {/* Buttons to edit or delete question but only if the user ID matches */}
+                    </div>
+                )
+
+            }
+
+
+
+            )}
         </div>
     )
 }
