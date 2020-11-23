1fedfca226058e921197342d3293c2c928402bb3
David Wallace <dwallacemarketing@gmail.com>
2020-11-22 20:57:51 -0500

    add regex to signup functionality

--- src/Components/Menu/SignUp.js
+++ src/Components/Menu/SignUp.js
@@ -34,8 +34,8 @@ export const SignUp = (props) => {
                     <Input type="text" name="username" value={username} placeholder="enter username " minLength="4" required onChange={(e) => setUsername(e.target.value)} />
                 </FormGroup>
                 <FormGroup>
-                    <label for="password">password</label>
-                    <Input type="password" name="password" value={password} placeholder="enter password" minLength="8"  required onChange={(e) => setPassword(e.target.value)}/>
+                    <label for="password" >Password</label>
+                    <Input  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" title="Please enter letters and numbers. Minimum length is 8 characters." type="password" name="password" value={password} placeholder="enter password" minLength="8" required onChange={(e) => setPassword(e.target.value)}/>
                 </FormGroup>
                 <Button type='submit'>Submit</Button>
             </Form>
